2bdeccc8556e6dfd85c5a88f546f27035a752b14
rmonteiro <ricardojorgesmonteiro@gmail.com>
2018-10-10 11:57:14 +0100

    added disparity calculation function using template matching

--- scalableTests/disparityCompensationSAI/disparityCompensationSAI_TM.m
+++ scalableTests/disparityCompensationSAI/disparityCompensationSAI_TM.m
@@ -1,4 +1,4 @@
-function [ dx, dy ] = disparityCompensationSAI_BM( im0, im1, window, blockSize, templateSize )
+function [ dx, dy ] = disparityCompensationSAI_TM( im0, im1, window, blockSize, templateSize )
 %% template matching between two frames to dertermine disparity
 
 [H,W]=size(im0);
@@ -7,6 +7,7 @@ bw = W / blockSize;
 
 dx = zeros(H,W);
 dy = zeros(H,W);
+pixelsTemplate = (blockSize + templateSize) * (blockSize + templateSize) - (blockSize * blockSize);
 
 for j = 1:bh
     for i = 1:bw
@@ -14,15 +15,44 @@ for j = 1:bh
         dispVectorY = 0;
         currentDistortion = Inf;
         bestDistortion = Inf;
+        %% test NULL vector
+        windH = 0;
+        windW = 0;
+        if (j-1)*blockSize + 1 - templateSize + windH > 0 && (j-1)*blockSize + blockSize + windH <= H && ...
+           (i-1)*blockSize + 1 - templateSize + windW > 0 && (i-1)*blockSize + blockSize + windW <= W && ...
+           (j-1)*blockSize + 1 - templateSize > 0 && (j-1)*blockSize + blockSize <= H && ...
+           (i-1)*blockSize + 1 - templateSize > 0 && (i-1)*blockSize + blockSize <= W
+           for py = 1-templateSize:blockSize
+               for px = 1-templateSize:blockSize
+                   if py <= 0 || px <= 0
+                       currentDistortion = currentDistortion + (im0((j-1)*blockSize + py,         (i-1)*blockSize + px) - ...
+                           im1((j-1)*blockSize + py + windH, (i-1)*blockSize + px + windW)).^2;
+                   end
+               end
+           end
+           currentDistortion = currentDistortion / ((blockSize + templateSize) * pixelsTemplate);
+           bestDistortion = currentDistortion;
+           dispVectorX = windW;
+           dispVectorY = windH;
+        end
+        %% test search window
         for windH = -window(1):window(1)
             %for windW = -window(2):0 %% searching the left view on the right view vectors should be negative
             for windW = -window(2):window(2)
-                if (j-1)*blockSize + 1 + windH > 0 && (j-1)*blockSize + blockSize + windH <= H && ...
-                   (i-1)*blockSize + 1 + windW > 0 && (i-1)*blockSize + blockSize + windW <= W
-                    currentDistortion = immse(im0((j-1)*blockSize + 1:(j-1)*blockSize + blockSize, ...
-                                                  (i-1)*blockSize + 1:(i-1)*blockSize + blockSize), ...
-                                              im1((j-1)*blockSize + 1 + windH:(j-1)*blockSize + blockSize + windH, ...
-                                                  (i-1)*blockSize + 1 + windW:(i-1)*blockSize + blockSize + windW));
+                currentDistortion = 0;
+                if (j-1)*blockSize + 1 - templateSize + windH > 0 && (j-1)*blockSize + blockSize + windH <= H && ...
+                   (i-1)*blockSize + 1 - templateSize + windW > 0 && (i-1)*blockSize + blockSize + windW <= W && ...
+                   (j-1)*blockSize + 1 - templateSize > 0 && (j-1)*blockSize + blockSize <= H && ...
+                   (i-1)*blockSize + 1 - templateSize > 0 && (i-1)*blockSize + blockSize <= W
+                    for py = 1-templateSize:blockSize
+                        for px = 1-templateSize:blockSize
+                            if py <= 0 || px <= 0
+                                currentDistortion = currentDistortion + (im0((j-1)*blockSize + py,         (i-1)*blockSize + px) - ...
+                                                                         im1((j-1)*blockSize + py + windH, (i-1)*blockSize + px + windW)).^2;
+                            end
+                        end
+                    end
+                    currentDistortion = currentDistortion / ((blockSize + templateSize) * pixelsTemplate);
                     if currentDistortion < bestDistortion
                         bestDistortion = currentDistortion;
                         dispVectorX = windW;
@@ -31,6 +61,7 @@ for j = 1:bh
                 end
             end
         end
+        %% write best output
         dx((j-1)*blockSize + 1:(j-1)*blockSize + blockSize, ...
            (i-1)*blockSize + 1:(i-1)*blockSize + blockSize) = dispVectorX;
         dy((j-1)*blockSize + 1:(j-1)*blockSize + blockSize, ...
--- scalableTests/disparityCompensationSAI/expandLF.m
+++ scalableTests/disparityCompensationSAI/expandLF.m
@@ -1,5 +1,7 @@
 clear all
+close all
 LF_Image_PVS_SCL = '/home/rmonteiro/PhD/Sequences/EPFL/4DLF_PVS_Scalable/I01_Bikes__Decoded_13x13_YUV444_10bpp.yuv';
+%LF_Image_PVS_SCL = '/home/rmonteiro/PhD/Scripts/HEVC_tests/HEVC_EPFL_4DLF_PVS_SCL_B_YUV444_10/4DLF_13x13_PVS_SCL_I01_YUV444_10/32/rec.yuv';
 
 num_Layers = 6;
 layerMask =  [ 6 6 4 6 3 6 4 6 3 6 4 6 6 ;
@@ -15,6 +17,14 @@ layerMask =  [ 6 6 4 6 3 6 4 6 3 6 4 6 6 ;
                4 6 2 6 4 6 2 6 4 6 2 6 4 ;
                6 5 6 5 6 5 6 5 6 5 6 5 6 ;
                6 6 4 6 3 6 4 6 3 6 4 6 6 ];
+           
+expandToLF7X =  [ 0 0 0 0 0 0 0 ;
+                  0 0 1 0 1 0 0 ;
+                  0 2 3 2 4 2 0 ;
+                  0 0 1 0 1 0 0 ;
+                  0 2 4 2 3 2 0 ;
+                  0 0 1 0 1 0 0 ;
+                  0 0 0 0 0 0 0 ];
            
 W_BUF = 8125; % buffer res
 H_BUF = 5642; % 13 * 434
@@ -43,6 +53,9 @@ LF_Buff_L12 =   zeros(miSizeL12 * H_PVS_wo_padding, miSizeL12 * W_PVS_wo_padding
 LF_Buff_L34 =   zeros(miSizeL34 * H_PVS_wo_padding, miSizeL34 * W_PVS_wo_padding, 3);
 LF_Buff_L56 =   zeros(miSizeL56 * H_PVS_wo_padding, miSizeL56 * W_PVS_wo_padding, 3);
 
+SAIUsage =      zeros(miSize, miSize);
+SAIDistance =   zeros(miSize, miSize);
+
 PVS(:,:,:,:) = -1;
 
 LF(:,:,:,:,:) = -1;
@@ -54,6 +67,9 @@ LF_Buff(:,:,:) = -1;
 LF_Buff_L12(:,:,:) = -1;
 LF_Buff_L34(:,:,:) = -1;
 LF_Buff_L56(:,:,:) = -1;
+
+SAIUsage(:,:) = -1;
+SAIDistance(:,:) = -1;
 
 f = fopen(LF_Image_PVS_SCL,'r');
 frameNum = 0;
@@ -70,24 +86,84 @@ for l = 1:num_Layers
                 PVS(frameNum + 1, :, :, 1) = uint16(Y(1:H_PVS,1:W_PVS));
                 PVS(frameNum + 1, :, :, 2) = uint16(U(1:H_PVS,1:W_PVS));
                 PVS(frameNum + 1, :, :, 3) = uint16(V(1:H_PVS,1:W_PVS));
+                % gen LFs for the different layers
                 LF(ypos,xpos,:,:,1) = uint16(Y(1:H_PVS_wo_padding,1:W_PVS_wo_padding));
                 LF(ypos,xpos,:,:,2) = uint16(U(1:H_PVS_wo_padding,1:W_PVS_wo_padding));
                 LF(ypos,xpos,:,:,3) = uint16(V(1:H_PVS_wo_padding,1:W_PVS_wo_padding));
-                LF_Buff = reconstruct_lenslet_img10_anyMISize(LF, miSize);
-                frameNum = frameNum + 1
-            end
-          
-            if l == 3
-                if ypos == 8 && xpos == 8 % frame 9 % use frame 0 and 1
-                    [ dx8, dy8 ] = disparityCompensationSAI_BM( squeeze(PVS(1,:,:,1)), squeeze(PVS(2,:,:,1)), [1 8], 8 );
-                    figure, imshow(dx8,[])
-                    figure, imshow(dy8,[])
-                    figure, histogram(dx8)
-                    figure, histogram(dy8)
+                LF_L12((ypos+1)/4,(xpos+1)/4,:,:,1) = uint16(Y(1:H_PVS_wo_padding,1:W_PVS_wo_padding));
+                LF_L12((ypos+1)/4,(xpos+1)/4,:,:,2) = uint16(U(1:H_PVS_wo_padding,1:W_PVS_wo_padding));
+                LF_L12((ypos+1)/4,(xpos+1)/4,:,:,3) = uint16(V(1:H_PVS_wo_padding,1:W_PVS_wo_padding));
+                LF_L34((ypos+1)/2,(xpos+1)/2,:,:,1) = uint16(Y(1:H_PVS_wo_padding,1:W_PVS_wo_padding));
+                LF_L34((ypos+1)/2,(xpos+1)/2,:,:,2) = uint16(U(1:H_PVS_wo_padding,1:W_PVS_wo_padding));
+                LF_L34((ypos+1)/2,(xpos+1)/2,:,:,3) = uint16(V(1:H_PVS_wo_padding,1:W_PVS_wo_padding));
+                LF_L56(ypos+1,xpos+1,:,:,1) = uint16(Y(1:H_PVS_wo_padding,1:W_PVS_wo_padding));
+                LF_L56(ypos+1,xpos+1,:,:,2) = uint16(U(1:H_PVS_wo_padding,1:W_PVS_wo_padding));
+                LF_L56(ypos+1,xpos+1,:,:,3) = uint16(V(1:H_PVS_wo_padding,1:W_PVS_wo_padding));
+                % generate LF buffers for the different layers 
+                for y = 1:H_PVS_wo_padding
+                    for x = 1:W_PVS_wo_padding
+                        if l <= 2
+                            LF_Buff_L12((ypos+1)/4 + (y-1)*miSizeL12, (xpos+1)/4 + (x-1)*miSizeL12, 1) = uint16(Y(y,x));
+                            LF_Buff_L12((ypos+1)/4 + (y-1)*miSizeL12, (xpos+1)/4 + (x-1)*miSizeL12, 2) = uint16(U(y,x));
+                            LF_Buff_L12((ypos+1)/4 + (y-1)*miSizeL12, (xpos+1)/4 + (x-1)*miSizeL12, 3) = uint16(V(y,x));
+                        end
+                        if l <= 4
+                            LF_Buff_L34((ypos+1)/2 + (y-1)*miSizeL34, (xpos+1)/2 + (x-1)*miSizeL34, 1) = uint16(Y(y,x));
+                            LF_Buff_L34((ypos+1)/2 + (y-1)*miSizeL34, (xpos+1)/2 + (x-1)*miSizeL34, 2) = uint16(U(y,x));
+                            LF_Buff_L34((ypos+1)/2 + (y-1)*miSizeL34, (xpos+1)/2 + (x-1)*miSizeL34, 3) = uint16(V(y,x));
+                        end
+                        if l <= 6
+                            LF_Buff_L56(ypos + (y-1)*miSizeL56, xpos + (x-1)*miSizeL56, 1) = uint16(Y(y,x));
+                            LF_Buff_L56(ypos + (y-1)*miSizeL56, xpos + (x-1)*miSizeL56, 2) = uint16(U(y,x));
+                            LF_Buff_L56(ypos + (y-1)*miSizeL56, xpos + (x-1)*miSizeL56, 3) = uint16(V(y,x));
+                        end
+                    end
                 end
+                
+                % apply disparity compensation to the current 
+                % calculate euclidean distance to current SAI
+                lowestDistanceSAIX = 7;
+                lowestDistanceSAIY = 7;
+                bestSAIDistance = Inf;
+                SAIDistance = Inf;
+                referenceSAI = 0;
+                for y = 1:miSize
+                    for x = 1:miSize
+                        if SAIUsage(y,x) == -1
+                            SAIDistance(y,x) = Inf;
+                        else
+                            SAIDistance(y,x) = sqrt((y-ypos)^2+(x-xpos)^2);
+                        end
+                    end
+                end
+                % select reference -> lowest distance and lowest frame
+                %                     number i.e. upper layers
+                for frame = 0:miSize*miSize-1
+                    for y = 1:miSize
+                        for x = 1:miSize
+                            if SAIUsage(y,x) == frame
+                                if  SAIDistance(y,x) < bestSAIDistance
+                                    referenceSAI = frame;
+                                    bestSAIDistance = SAIDistance(y,x);
+                                end
+                            end
+                        end
+                    end
+                end %                                    currSAI - fixed template      | refSAI - test several disparity vectors
+                [ dx dy ] = disparityCompensationSAI_TM( squeeze(PVS(frameNum+1,:,:,1)), squeeze(PVS(referenceSAI+1,:,:,1)), [8 8], 8, 4);
+                %referenceSAI
+                %frameNum
+                %figure, imshow(dx,[]);
+                %figure, imshow(dy,[]);
+                
+                
+                    
+                   
+                
+                SAIUsage(ypos,xpos) = frameNum;
+                frameNum = frameNum + 1;
+                %close all
             end
-            
-            
         end
     end
 end
